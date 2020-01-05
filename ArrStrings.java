b4067beb61ad056174263f3ae1ed98f832acae36
Akash Jag <akashjagannathan408@gmail.com>
2020-01-04 17:51:50 -0800

    one problem

--- ArrStrings.java
+++ ArrStrings.java
@@ -1,12 +1,19 @@
 public class ArrStrings{
 
-  public boolean isUnique(String str){
-
+  static boolean isUnique(String str){
+    for(int i = 0; i<str.length(); i++){
+      for(int j = 0; j<str.length(); j++){
+        if(str.charAt(i) == str.charAt(j) && i != j){
+          return false;
+        }
+      }
+    }
+    return true;
   }
+  
   public static void main(String[] args) {
-      Integer a = 10;
-      int b = 10;
-      a = null;
-      b = null;
+    String test = "helo";
+    Boolean test1 = isUnique(test);
+    System.out.println(test1);
   }
 }
