8e98be20493a949656636678e9e36324b54d61a6
Akash Jag <akashjagannathan408@gmail.com>
2020-01-05 12:35:55 -0800

    working on arr strings still

--- ArrStrings.java
+++ ArrStrings.java
@@ -1,19 +1,34 @@
 public class ArrStrings{
 
-  static boolean isUnique(String str){
-    for(int i = 0; i<str.length(); i++){
-      for(int j = 0; j<str.length(); j++){
-        if(str.charAt(i) == str.charAt(j) && i != j){
-          return false;
-        }
-      }
-    }
-    return true;
+  // static boolean isUnique(String str){
+  //   for(int i = 0; i<str.length(); i++){
+  //     for(int j = 0; j<str.length(); j++){
+  //       if(str.charAt(i) == str.charAt(j) && i != j){
+  //         return false;
+  //       }
+  //     }
+  //   }
+  //   return true;
+  // }
+
+  // static boolean perm(String one, String two){
+  //   //double for loop O(n2)
+  //   //one for loop with binary search: O(n log n)
+  //
+  // }
+
+  // static String urlify(String spaced){
+  //   //for loop, every space use a string replace
+  // }
+
+  static boolean palin(String str){
+    //even number of every character, besides one if string length is odd
+    //
   }
-  
+
   public static void main(String[] args) {
-    String test = "helo";
-    Boolean test1 = isUnique(test);
-    System.out.println(test1);
+    // String test = "helo";
+    // Boolean test1 = isUnique(test);
+    // System.out.println(test1);
   }
 }
